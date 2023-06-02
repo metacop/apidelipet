77bae19fb6c8acdcba66d44e7bcd183bf2e24c5f
Carlos Terrazas <carlos.terrazasv@gmail.com>
2023-06-02 04:40:41 -0400

    se modifico la estructura de la base de datos de mascota imagen null

--- database/migrations/2021_12_19_201451_mascota_table.php
+++ database/migrations/2021_12_19_201451_mascota_table.php
@@ -20,7 +20,7 @@ class MascotaTable extends Migration
             $table->string('sexo');
             $table->integer('altura');
             $table->integer('peso');
-            $table->string('imagen_mascota');
+            $table->string('imagen_mascota')->nullable();;
             $table->date('fecha_nacimiento');
             $table->timestamps();
 
